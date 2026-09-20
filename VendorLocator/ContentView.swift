fab2259a32b0dc37cff02b613d8b39acb9ed3411
Ndabe <ndabenhlelanga@gmail.com>
2026-09-20 10:43:23 +0200

    Initial Commit

---
+++ VendorLocator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VendorLocator
+//
+//  Created by Ndabenhle Langa on 2026/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VendorLocator/VendorLocatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  VendorLocatorApp.swift
+//  VendorLocator
+//
+//  Created by Ndabenhle Langa on 2026/09/20.
+//
+
+import SwiftUI
+
+@main
+struct VendorLocatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
